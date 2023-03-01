fe9d04e799a5a1cc3992844b4dd77b2a87253bfe
kabomboExe <kafehbre@gmail.com>
2023-03-01 18:10:51 +0100

    portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Portfolio.css";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WebAssetIcon from '@mui/icons-material/WebAsset';
 
 const Portfolio = () => {
     return <div className='container'>
@@ -7,7 +9,17 @@ const Portfolio = () => {
             <iframe seamless="seamless" scrolling='no' className='iframe' title='netflix-clone' src='https://netflix-clone-kafehbre.netlify.app'></iframe>
             <div className='overlay'></div>
             <div className='infos'>
-                <h1 className='text'>Test</h1>
+                <h1 className='info-heading'>Netflix Clone</h1>
+                <p className='text'>I tried to mimic the design of the streaming platform Netflix with React. Here I worked with different hooks, state management, fetched different data from movies and series through an API (themoviedb API) and more...</p>
+                <a className='info-text' href='https://netflix-clone-kafehbre.netlify.app'>
+                    <WebAssetIcon ></WebAssetIcon>
+                    <p className='link-text'>Website Link</p>
+                </a>
+                <a className='info-text' href='https://github.com/kabomboExe/netflix-clone'>
+                    <GitHubIcon ></GitHubIcon>
+                    <p className='link-text'>Repository Link</p>
+                </a>
+
             </div>
         </div>
     </div>;
