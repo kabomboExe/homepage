0bf5834ef1bd61f92ba790b0405757ee13823229
kabomboExe <kafehbre@gmail.com>
2023-03-09 16:04:18 +0100

    portfolio card orientation

--- src/components/PortfolioInfoCard.js
+++ src/components/PortfolioInfoCard.js
@@ -4,30 +4,69 @@ import WebAssetIcon from "@mui/icons-material/WebAsset";
 import "./PortfolioInfoCard.css";
 
 const PortfolioInfoCard = (props) => {
+  const cardOrientation = props.orientation;
+
   return (
     <div className="info-container">
-      <h1 className="info-heading">{props.heading}</h1>
-      <div className="website-card">
-        <iframe
-          seamless="seamless"
-          scrolling="no"
-          className="iframe"
-          title="netflix-clone"
-          src={props.website}
-        ></iframe>
-        <div className="overlay"></div>
-        <div className="infos">
-          <p className="text">{props.infoText}</p>
-          <a className="info-text" href={props.website}>
-          <div className="info-icon"><WebAssetIcon></WebAssetIcon></div>
-            <p className="link-text">Website Link</p>
-          </a>
-          <a className="info-text" href={props.github}>
-            <div className="info-icon"><GitHubIcon ></GitHubIcon></div>
-            <p className="link-text">Repository Link</p>
-          </a>
-        </div>
-      </div>
+      {cardOrientation === "left" ? (
+        <>
+          <h1 className="info-heading">{props.heading}</h1>{" "}
+          <div className="website-card">
+            <iframe
+              seamless="seamless"
+              scrolling="no"
+              className="iframe"
+              title="netflix-clone"
+              src={props.website}
+            ></iframe>
+            <div className="overlay"></div>
+            <div className="infos">
+              <p className="text">{props.infoText}</p>
+              <a className="info-text" href={props.website}>
+                <div className="info-icon">
+                  <WebAssetIcon></WebAssetIcon>
+                </div>
+                <p className="link-text">Website Link</p>
+              </a>
+              <a className="info-text" href={props.github}>
+                <div className="info-icon">
+                  <GitHubIcon></GitHubIcon>
+                </div>
+                <p className="link-text">Repository Link</p>
+              </a>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="info-heading right">{props.heading}</h1>{" "}
+          <div className="website-card">
+            <iframe
+              seamless="seamless"
+              scrolling="no"
+              className="iframe"
+              title="netflix-clone"
+              src={props.website}
+            ></iframe>
+            <div className="overlay"></div>
+            <div className="infos-to-left">
+              <p className="text">{props.infoText}</p>
+              <a className="info-text" href={props.website}>
+                <div className="info-icon">
+                  <WebAssetIcon></WebAssetIcon>
+                </div>
+                <p className="link-text">Website Link</p>
+              </a>
+              <a className="info-text" href={props.github}>
+                <div className="info-icon">
+                  <GitHubIcon></GitHubIcon>
+                </div>
+                <p className="link-text">Repository Link</p>
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Portfolio.css";
 import PortfolioInfoCard from "../components/PortfolioInfoCard";
 import { netflixCloneText, calculatorText } from "../data/portfolio-texts";
 
 const Portfolio = () => {
+  const leftOrientation = "left";
+  const rightOrientation = "right";
+
+
   return (
     <div className="container portfolio">
-      <PortfolioInfoCard
-        heading="Netflix Clone"
-        website="https://netflix-clone-kafehbre.netlify.app"
-        github="https://github.com/kabomboExe/netflix-clone"
-        infoText={netflixCloneText}
-      ></PortfolioInfoCard>
-      <PortfolioInfoCard
-        heading="Calculator"
-        website="https://calculator-kafehbre.netlify.app"
-        github="https://github.com/kabomboExe/Calculator"
-        infoText={calculatorText}
-      ></PortfolioInfoCard>
+      <div className={leftOrientation}>
+        <PortfolioInfoCard
+          heading="Netflix Clone"
+          website="https://netflix-clone-kafehbre.netlify.app"
+          github="https://github.com/kabomboExe/netflix-clone"
+          infoText={netflixCloneText}
+          orientation={leftOrientation}
+        ></PortfolioInfoCard>
+      </div>
+      <div className={rightOrientation}>
+        <PortfolioInfoCard
+          heading="Calculator"
+          website="https://calculator-kafehbre.netlify.app"
+          github="https://github.com/kabomboExe/Calculator"
+          infoText={calculatorText}
+          orientation={rightOrientation}
+        ></PortfolioInfoCard>
+      </div>
     </div>
   );
 };
